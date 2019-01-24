61019f30fe778b71bddd3facab8bcbc717adc248
Nikhil <the@realest.ninja>
2019-01-24 19:38:52 +0100

    Cleanup

--- preScript.sh
+++ preScript.sh
@@ -24,8 +24,10 @@ echo 'Done.'
 
 echo 'Creating SSH-Key'
 ssh-keygen -t rsa -b 4096 -C "$email"
+
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
+
 xclip -sel clip < ~/.ssh/id_rsa.pub
 echo 'Done.'
 
