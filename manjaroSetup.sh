8397589f95c94ea7c496a18c6712e15d82b58e82
Nikhil Nawgiri <the@realest.ninja>
2020-11-21 19:12:06 +0100

    Add rofi-emoji

--- manjaroSetup.sh
+++ manjaroSetup.sh
@@ -251,6 +251,7 @@ if [[ $do_software == y* ]]; then
   yay -S --noconfirm spotify
   yay -S --noconfirm unimatrix-git
   yay -S --noconfirm light-git
+  yay -S --noconfirm rofi-emoji
   # yay -S --noconfirm slack-desktop
   # yay -S --noconfirm jmtpfs
   # yay -S --noconfirm gucharmap
